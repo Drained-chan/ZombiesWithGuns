83ec70ab3736bbe9d2701dbb21cf240e98113089
Charles O <mcfx2@mcfx2.co>
2021-08-12 21:13:09 -0700

    added some rotation helpers to mathutils

--- Assets/Scripts/Utility/MathUtils.cs
+++ Assets/Scripts/Utility/MathUtils.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public static class MathUtils
 {
+    /// <summary>
+    /// Math operations for modifying and creating vectors.
+    /// </summary>
     public static class Vector
     {
         /// <summary>
@@ -16,9 +19,41 @@ public static class MathUtils
             return rotation * Vector3.forward;
         }
 
+        /// <summary>
+        /// Given a rotation, returns a unit vector pointing in the same direction.
+        /// </summary>
+        /// <param name="rotation">Euler representation of the angle to point along.</param>
+        /// <returns>Unit vector pointing the same way as <paramref name="rotation"/>.</returns>
         public static Vector3 FromRotation(Vector3 rotation)
         {
             return FromRotation(Quaternion.Euler(rotation));
+        }
+    }
+
+
+    /// <summary>
+    /// Math operations for modifying and creating rotations.
+    /// </summary>
+    public static class Rotation
+    {
+        /// <summary>
+        /// Given a vector, returns a rotation oriented the same way.
+        /// </summary>
+        /// <param name="vector">Vector pointing in the direction the rotation should point.</param>
+        /// <returns>A quaternion representation of the rotation pointing the same way as <paramref name="vector"/>.</returns>
+        public static Quaternion FromVector(Vector3 vector)
+        {
+            return new Quaternion(vector);
+        }
+
+        /// <summary>
+        /// Given a vector, returns a rotation oriented the same way.
+        /// </summary>
+        /// <param name="vector">Vector pointing in the direction the rotation should point.</param>
+        /// <returns>A quaternion representation of the rotation pointing the same way as <paramref name="vector"/>.</returns>
+        public static Quaternion FromVector(Vector2 vector)
+        {
+            return new Quaternion(vector);
         }
     }
 }
