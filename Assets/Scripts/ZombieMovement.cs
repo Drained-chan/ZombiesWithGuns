5aaec24baa03d03290820279db56722818e83fba
Isonzo <renato.soverina@gmail.com>
2021-08-12 23:32:04 -0500

    Added Waypoint() function that returns a simple waypoint to use as reference for wandering AI- The variable wanderTimeCounter has been created to time the Waypoint() function's calls.

--- Assets/Scripts/ZombieMovement.cs
+++ Assets/Scripts/ZombieMovement.cs
@@ -5,18 +5,47 @@ using UnityEngine;
 public class ZombieMovement : MonoBehaviour
 {
     Transform target;
-    public float Speed = 4;
+    public float Speed = 4f;
+    [SerializeField] private float range = 5f;
+    [SerializeField] private float wanderTime = 5f;
+    private float wanderTimeCounter;
+    private bool isChasing = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        wanderTimeCounter = wanderTime;
         target = GameObject.Find("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.right = target.transform.position - transform.position;
+        // Once wander time expires, set a new waypoint and reset the timer
+        wanderTimeCounter -= Time.deltaTime;
+        if (wanderTimeCounter < 0)
+        {
+            wanderTimeCounter = wanderTime;
+            var waypoint = Waypoint(range);
+        }
+        float distance = Vector3.Distance(target.transform.position, gameObject.transform.position);
+        
         transform.position = Vector2.MoveTowards(transform.position, target.position, Speed * Time.deltaTime);
+
+        
+    }
+
+
+    // Waypoint returns two floats, so we can use them as coordinates.
+    (float, float) Waypoint(float range) //Returns two floats for use as a waypoint
+    {
+        float x = Random.Range(-range, range);
+        float y = Random.Range(-range, range);
+        return (x, y);
+    }
+    void Wander(float x, float y)
+    {
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
