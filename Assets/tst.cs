faaf35b967bd81f147fbd50fe31292387ddf6339
isaacKillalea <isaackillalea@gmail.com>
2021-08-14 16:32:24 +1000

    Forgot to add files, android studio has had me spoilt

---
+++ Assets/tst.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+public class tst : MonoBehaviour
+{
+	public Tilemap floor;
+    // Start is called before the first frame update
+    void Awake()
+    {
+		//Debug.Log("size: x" + floor.size.x + " y" + floor.size.y);
+		//floor.ClearAllTiles(); 
+		//Debug.Log("size: x" + floor.size.x + " y" + floor.size.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
