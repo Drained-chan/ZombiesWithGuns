2f5459185cf7b00815fc4bfe34cfb4a59a0fea12
Charles O <mcfx2@mcfx2.co>
2021-08-18 21:47:33 -0700

    Added basic script plus example, just need to add a movement example

---
+++ Assets/Scripts/Zombie/ZombieMovementBase.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Derive your zombie movement behaviors from this.
+/// </summary>
+[RequireComponent(typeof(ZombieShootBase))]
+public abstract class ZombieMovementBase : MonoBehaviour
+{
+    ZombieShootBase shootBase;
+
+    /// <summary>
+    /// If you use Start() in your behavior, you MUST call base.Start()!!!!
+    /// </summary>
+    virtual protected void Start()
+    {
+        shootBase = GetComponent<ZombieShootBase>();
+    }
+
+
+    /// <summary>
+    /// You must implement this method.
+    /// </summary>
+    /// <returns>true if the zombie is currently moving, false otherwise.</returns>
+    abstract public bool IsMoving();
+
+    /// <summary>
+    /// This is provided by the shooting behavior.
+    /// </summary>
+    /// <returns>true if the zombie is shooting or setting up a shot, false otherwise.</returns>
+    protected bool IsShooting()
+    {
+        return shootBase.IsShooting();
+    }
+}
---
+++ Assets/Scripts/Zombie/ZombieShootBase.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ZombieShootBase : MonoBehaviour
+{
+    ZombieMovementBase moveBase;
+
+    /// <summary>
+    /// If you use Start() in your behavior, you MUST call base.Start()!!!!
+    /// </summary>
+    virtual protected void Start()
+    {
+        moveBase = GetComponent<ZombieMovementBase>();
+
+        if(!moveBase)
+        {
+            DebugUtils.CrashAndBurn("Movement base of some kind is required on " + gameObject.name + "!!!!!");
+        }
+    }
+
+
+    /// <summary>
+    /// This is provided by the moving behavior.
+    /// 
+    /// </summary>
+    /// <returns>true if the zombie is currently moving, false otherwise.</returns>
+    protected bool IsMoving()
+    {
+        return moveBase.IsMoving();
+    }
+
+    /// <summary>
+    /// You must implement this method.
+    /// </summary>
+    /// <returns>true if the zombie is shooting or setting up a shot, false otherwise.</returns>
+    abstract public bool IsShooting();
+}
