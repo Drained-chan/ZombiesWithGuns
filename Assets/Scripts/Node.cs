4458341fe7c1b9998c55acd9caa8dc3622a05a08
Isonzo <renato.soverina@gmail.com>
2021-08-20 20:35:48 -0500

    Gave nodes their x and y as properties for use in GetNeighbours()

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -6,14 +6,18 @@ public class Node
 {
     public bool walkable;
     public Vector2 worldPosition;
+    public int gridX;
+    public int gridY;
 
     public int gCost;
     public int hCost;
 
-    public Node(bool _walkable, Vector2 _worldPos)
+    public Node(bool _walkable, Vector2 _worldPos, int _gridX, int _gridY)
     {
         walkable = _walkable;
         worldPosition = _worldPos;
+        gridX = _gridX;
+        gridY = _gridY;
     }
     public int fCost
     {
