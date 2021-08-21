d9d31b86c8bbdc025a1339f9923413500ec8cb87
Isonzo <renato.soverina@gmail.com>
2021-08-20 19:34:12 -0500

    Working grid system that detects walls (kind of, there's some bugs)

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -9,8 +9,44 @@ public class Grid : MonoBehaviour
     public float nodeRadius;
     Node[,] grid;
 
+    float nodeDiameter;
+    int gridSizeX, gridSizeY;
+
+    private void Start()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        CreateGrid();
+    }
+
+    void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.up * gridWorldSize.y / 2;
+        for (int x = 0; x < gridSizeX; x++)
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.up * (y * nodeDiameter + nodeRadius);
+                bool walkable = !(Physics2D.OverlapCircle(worldPoint, nodeRadius, unwalkableMask));
+                grid[x, y] = new Node(walkable, worldPoint);
+            }
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, gridWorldSize.y, 1));
+
+        if (grid != null)
+        {
+            foreach (Node n in grid)
+            {
+                if (n.walkable)
+                    Gizmos.color = Color.white;
+                else
+                    Gizmos.color = Color.red;
+                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter-.1f));
+            }
+        }
     }
 }
--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class Node
 {
     public bool walkable;
-    public Vector2 worldPosition;
+    public Vector3 worldPosition;
 
-    public Node(bool _walkable, Vector2 _worldPos)
+    public Node(bool _walkable, Vector3 _worldPos)
     {
         walkable = _walkable;
         worldPosition = _worldPos;
