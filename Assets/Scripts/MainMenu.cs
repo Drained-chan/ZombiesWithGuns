7d352568571ecc8769f52144e581794ab773583b
Isonzo <renato.soverina@gmail.com>
2021-08-24 23:14:03 -0500

    Added template MainMenu script and the ability to go into an options menu (no actual options added)

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        Debug.Log("You're now a player.");
+    }
+
+    public void QuitButton()
+    {
+        Debug.Log("Game's closed.");
+    }
+}
